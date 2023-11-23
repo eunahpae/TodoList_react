44b83c5b2dbe110a1b5d2939db2f0d2ed07171a7
eunahpae <eunahp86@gmail.com>
2023-11-23 14:43:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+/* eslint-disable */
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [todolist, setTodolist] = useState('');
+  let [text, setText] = useState('');
+  let [title, setTitle] = useState('');
+  let [list, setList] = useState(false);
+
+
   return (
     <div className="App">
       <div className="navbar">
         <div>Todo List</div>
-      </div>
-    </div>
+      </div>  
 
+      <Lists todolist={todolist} />
+
+      <input placeholder='할 일' onChange={(e)=>{
+        setText(e.target.value)
+      }} />
+      <button onClick={()=>{
+        let copy = [...todolist];
+        copy.unshift(text);
+        setTodolist(copy);
+      }}>등록</button>
+
+    </div>
   );
 }
+
+
+function Lists() {
+  return (
+    <div className='lists'>
+      <h4><input type='checkbox' /> todolist
+        <button>삭제</button></h4>
+      <hr />
+    </div>
+  )
+}
+
 
 export default App;
